1e30c803cfa80b9d86575675fe6868acba7866ee
soonmingwak <kwaksh20@naver.com>
2021-11-25 09:45:55 +0900

    2021-11-25 09:45

--- src/main/java/com/bbs/controller/MainController.java
+++ src/main/java/com/bbs/controller/MainController.java
@@ -19,30 +19,18 @@ import com.bbs.service.UsersService;
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
-	
-	@Inject
-	UsersService service;
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+public class MainController {
+
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) throws Exception {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		service.check_id(null);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
 	
+
+		return "main/main";
+	}
+
 }
